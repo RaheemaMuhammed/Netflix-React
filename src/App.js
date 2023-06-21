61c6b165a6eb7803c9945d9c45ef4912dfae3767
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2023-06-21 15:45:56 +0530

    dynamic rendering

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/NavBar/Banner/Banner';
 import RowPost from './components/RowPost/RowPost';
-
+import {originals,action,horror} from './urls'
 function App() {
   return (
     <div className='App'>
   <NavBar/>
   <Banner/>
-  <RowPost/>
+  <RowPost url={originals} title='Netflix Originals'/>
+  <RowPost url={action} title='Action' isSmall />
+  <RowPost url={horror} title='Horror' isSmall />
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+ export const baseUrl='https://api.themoviedb.org/3'
+ export const imageUrl='https://image.tmdb.org/t/p/original'
+ export const API_KEY='a0b1c4665df378b96c2706c75c3d247c'
